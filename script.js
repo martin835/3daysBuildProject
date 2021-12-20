543da0d1fecc1f8c66f58b4c1b6b0bee17408d4d
martin835 <64438132+martin835@users.noreply.github.com>
2021-12-20 17:33:30 +0100

    navbar almost done

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const showInput = function () {
+        let inputFieldDiv = document.querySelector('.input-div')
+        let inputField = document.querySelector('.input-div input')        
+        inputFieldDiv.classList.toggle('slideleft')
+        inputField.classList.toggle('hidden-input')
+}
+
