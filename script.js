5cd261518dbbf026ae72d7ab9e81031b4e9551dc
martin835 <64438132+martin835@users.noreply.github.com>
2021-12-21 18:28:21 +0100

    navbar responsive done

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 const showInput = function () {
-        let inputFieldDiv = document.querySelector('.input-div')
-        let inputField = document.querySelector('.input-div input')        
-        inputFieldDiv.classList.toggle('slideleft')
-        inputField.classList.toggle('hidden-input')
+        let mq = window.matchMedia( "(max-width: 904px)" );
+        if (mq.matches) {
+                // window width is at less than 904px
+                let inputContainer = document.getElementById("container-input-mobile")
+                inputContainer.classList.toggle('show-mobile-search')
+        }
+        else {
+                // window width is greater than 904px
+                let inputFieldDiv = document.querySelector('.input-div')
+                let inputField = document.querySelector('.input-div input')         
+                inputFieldDiv.classList.toggle('slideleft')
+                inputField.classList.toggle('hidden-input')        
+                inputField.focus()
+        }        
 }
 
