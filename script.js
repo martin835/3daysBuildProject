75e83379227c4c561ffd0bfb470c537c95807e07
martin835 <64438132+martin835@users.noreply.github.com>
2021-12-22 16:13:44 +0100

    programmatic cards generation

--- script.js
+++ script.js
@@ -18,3 +18,53 @@ const showInput = function () {
         }        
 }
 
+const getContent = function () {
+        let parent = document.getElementById('mcard-parent')
+        let imgUrl = 'assets\\image1.png'
+        let avatarUrl = 'assets\\avatar.png'
+        let card = `
+        <div class="row  mcard mb-5"> 
+        <div class="mcard-content col-8">
+            <div class="author">
+              <a href="">
+                  <img src="${avatarUrl}" alt="author" class="author-avatar">
+                  <span class="author-text ml-1">Astroport</span>
+                </a>
+                <a href="">
+                  <h2 class="mt-2">Olympus DAO and other Rebase Tokens. Failed experiment or opportunity of a lifetime?</h2>
+                  <p>A good API architecture is important in effectively handling the communication between microservices. Don\'t be afraid to create new microservices, and try to decouple functionality as much as possible. </p>
+                </a>
+              </div>
+              <div class="mcard-tags d-flex justify-content-between  ">
+                <div class="mcard-tags-left align-self-center">
+                  <span>Aug 28</span>
+                  <i class="bi bi-dot"></i>
+                  <span>6 min read</span>
+                  <i class="bi bi-dot d552"></i>
+                  <span class="mtag d552">Telegram Bot</span>
+                  <i class="bi bi-dot d552"></i>
+                  <span class="d552">Popular on Medium</span>
+                  <i class="bi bi-star-fill d552"></i>
+                </div>
+                <div class="mcard-tags-right">
+                  <i class="bi bi-bookmark-plus"></i>
+                  <i class="bi bi-three-dots"></i>
+                </div>
+              </div>
+         </div>
+        <div class="mcard-img col-4">
+          <img src="${imgUrl}" class="" alt=""card image>
+        </div>
+  </div>
+        `;
+
+        for (let i = 0; i < 10; i++) {
+                parent.innerHTML += card
+                
+        }
+}
+
+window.onload = function () {
+        getContent()
+}
+
